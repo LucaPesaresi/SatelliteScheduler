88ab2d59bea671b8cb6a62ca237b60cfeef5b66c
liuk26712 <luca.pesaresi2@studio.unibo.it>
2021-03-10 15:09:01 +0100

    Aggiungere i file di progetto.

---
+++ SatelliteScheduler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SatelliteScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
